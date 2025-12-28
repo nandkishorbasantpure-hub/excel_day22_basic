9fd7a2ff65203c5d8b2ba67089a86259f340b6d1
Nandkishor Basantpure <nandkishorbasantpure@gmail.com>
2025-12-28 17:50:59 +0530

    Add files via upload

---
+++ excel_day22_basic.xlsx.py
@@ -0,0 +1,17 @@
+Python 3.12.10 (tags/v3.12.10:0cc8128, Apr  8 2025, 12:21:36) [MSC v.1943 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> import pandas as pd
+
+>>> import numpy as np
+>>> import numpy as np
+>>> data = {
+...     "Hours_Studied": [2,4,6,8],
+...     "Marks": [50,65,80,90]
+...     }
+>>> df = pd.DataFrame(data)
+>>> print(df)
+   Hours_Studied  Marks
+0              2     50
+1              4     65
+2              6     80
+3              8     90
